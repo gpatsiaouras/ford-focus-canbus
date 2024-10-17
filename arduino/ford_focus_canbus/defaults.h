5991b56aae8080aa902ceb620daa06226ff7d94c
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2024-10-17 21:52:32 +0200

    Added support for two cameras. Second camera is activated
    by button also decoded a lot of the canbus messages

--- arduino/ford_focus_canbus/defaults.h
+++ arduino/ford_focus_canbus/defaults.h
@@ -1,22 +1,90 @@
 // Connections
 const int in_illum_pin = 3;
+const int in_second_cam_button_pin = 4;
 const int out_illum_pin = 9;
 const int reverse_trigger_pin = 8;
 const int park_brake_trigger_pin = 7;
+const int can_int_pin = 2;
+const int cameras_relay = 5;
+
+// Constants
+// Due to optocouplers values are reversed
+const int ACTIVE = 0;
+const int INACTIVE = 1;
 
 union Status
 {
   struct __attribute__((__packed__)) //packed means that the struct will have exactly the ssize of bytes it contains
   {
-    uint8_t first_byte;
+    uint8_t unknown_1:1;
+    uint8_t unknown_2:1;
+    uint8_t unknown_3:1;
+    uint8_t trunk_open:1;
+    uint8_t codrivers_rear_door_open:1;
+    uint8_t drivers_rear_door_open:1;
+    uint8_t codrivers_door_open:1;
+    uint8_t drivers_door_open:1;
+
     uint8_t second_byte;
     uint8_t third_byte;
 
     //fourth byte split two bits
     uint8_t parking_en:1;
     uint8_t reverse_en:1;
-    uint8_t:6; //reserved
+    uint8_t engine_on:1;
+    uint8_t ac_on:1;
+    uint8_t clima_on:1;
+    uint8_t rear_window_defrost_on:1;
+    uint8_t high_beam_on:1;
+    uint8_t lights_on:1;
+    // uint8_t:6; //reserved
     
+    uint8_t fifth_byte;
+
+    uint8_t:4; //reserved
+    uint8_t lock_en:1;
+    uint8_t unlock_en:1;
+    uint8_t:2; //reserved
+
+    uint8_t seventh_byte;
+    uint8_t eighth_byte;
+  };
+  uint8_t bytes[8];
+};
+
+union EngineStatus
+{
+  struct __attribute__((__packed__)) //packed means that the struct will have exactly the ssize of bytes it contains
+  {
+    uint16_t rpm; // x4
+    uint8_t third_byte;
+     // speed=(XX-10,000)/100; 0x2710=stopped	
+    uint8_t fourth_byte;
+    uint16_t speed;
+    uint16_t throttle;
+  };
+  uint8_t bytes[8];
+};
+
+union SeatbeltsStatus
+{
+  struct __attribute__((__packed__)) //packed means that the struct will have exactly the ssize of bytes it contains
+  {
+    uint8_t first_byte;
+
+    //second byte split two bits
+    uint8_t bit_1:1;
+    uint8_t bit_2:1;
+    uint8_t bit_3:1;
+    uint8_t bit_4:1;
+    uint8_t bit_5:1;
+    uint8_t drivers_seatbelt:1;
+    uint8_t codriver_seatbelt:1;
+    uint8_t bit_8:1;
+    // uint8_t:6; //reserved
+    
+    uint8_t third_byte;
+    uint8_t fourth_byte;
     uint8_t fifth_byte;
     uint8_t sixth_byte;
     uint8_t seventh_byte;
@@ -25,5 +93,46 @@ union Status
   uint8_t bytes[8];
 };
 
+union SomethingStatus
+{
+  struct __attribute__((__packed__)) //packed means that the struct will have exactly the ssize of bytes it contains
+  {
+    uint8_t first_bit_1:1;
+    uint8_t first_bit_2:1;
+    uint8_t first_bit_3:1;
+    uint8_t first_bit_4:1;
+    uint8_t first_bit_5:1;
+    uint8_t first_bit_6:1;
+    uint8_t first_bit_7:1;
+    uint8_t first_bit_8:1;
+    uint8_t second_bit_1:1;
+    uint8_t second_bit_2:1;
+    uint8_t second_bit_3:1;
+    uint8_t second_bit_4:1;
+    uint8_t second_bit_5:1;
+    uint8_t second_bit_6:1;
+    uint8_t second_bit_7:1;
+    uint8_t second_bit_8:1;
+    uint8_t third_bit_1:1;
+    uint8_t hazard_lights_off:1; //meaning when on bit is 0
+    uint8_t third_bit_3:1;
+    uint8_t unlock_key_command:1;
+    uint8_t trunk_open_command:1;
+    uint8_t third_bit_6:1;
+    uint8_t third_bit_7:1;
+    uint8_t third_bit_8:1;
+    
+  };
+  uint8_t bytes[3];
+};
+
 // CanBus
-const unsigned long CAN_ID_STATE_PARK_REV = 433;
+const unsigned long CAN_ID_STATE_VEHICLE = 0x433;
+const unsigned long CAN_ID_STATE_ENGINE = 0x201;
+// 7th and 8th byte is throttle pedal position
+const unsigned long CAN_ID_STATE_PEDALS = 0x285;
+// 1st byte 8 bit cluch pedal
+// 2nd byte 8 bit brake pedal
+const unsigned long CAN_ID_STATE_LOCK = 0x210;
+const unsigned long CAN_ID_STATE_SEATBELTS = 0x460;
+const unsigned long CAN_ID_STATE_SOMETHING = 0x501;
--- arduino/ford_focus_canbus/ford_focus_canbus.ino
+++ arduino/ford_focus_canbus/ford_focus_canbus.ino
@@ -1,67 +1,119 @@
 #include <mcp_can.h>
 #include <SPI.h>
-#include "PWM.hpp"
 #include "defaults.h"
 
-PWM illum_pwm(in_illum_pin);
-
+// Can bus related variables
 long unsigned int rxId;
 unsigned char len = 0;
 unsigned char rxBuf[8];
+char msgString[128];
 #define CAN0_INT 2
 MCP_CAN CAN0(10);
+
+// Status
 Status status;
+SeatbeltsStatus seatbelts_status;
+SomethingStatus something_status;
+EngineStatus engine_status;
 
+// Camera system
+bool reverse_trigger_override = false;
+bool reverse_gear_engaged = false;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  
+
   pinMode(out_illum_pin, OUTPUT);
   pinMode(reverse_trigger_pin, OUTPUT);
   pinMode(park_brake_trigger_pin, OUTPUT);
-  
+  pinMode(cameras_relay, OUTPUT);
+  pinMode(in_second_cam_button_pin, INPUT);
+
   // Defaults
   digitalWrite(reverse_trigger_pin, HIGH);
   digitalWrite(park_brake_trigger_pin, LOW);
-  
-  // Initiate illumination pwm reader
-  illum_pwm.begin(false);
 
   // Initiate canbus
-  if(CAN0.begin(MCP_ANY, CAN_125KBPS, MCP_8MHZ) == CAN_OK) {
+  if (CAN0.begin(MCP_ANY, CAN_125KBPS, MCP_8MHZ) == CAN_OK)
+  {
     Serial.println("MCP2515 Initialized Successfully!");
-  } else {
+  }
+  else
+  {
     Serial.println("Error Initializing MCP2515...");
   }
   CAN0.setMode(MCP_NORMAL);
   pinMode(CAN0_INT, INPUT);
 }
 
-void debug_print_illum() {
-  Serial.print(illum_pwm.getValue());
-  Serial.print(" - ");
-  Serial.print(illum_pwm.getAge());
-  Serial.print(" - ");
+int camera_button_is_pressed() {
+  return !digitalRead(in_second_cam_button_pin);
 }
 
-void set_illum() {
-  if (illum_pwm.getValue() > 0 && illum_pwm.getAge() < 100000) {
-    digitalWrite(out_illum_pin, LOW);
-  } else {
-    digitalWrite(out_illum_pin, HIGH);
-  }
-}
-
-void loop() {
-  set_illum();
-  // debug_print_illum();
-
-  if(!digitalRead(CAN0_INT)) {
+void read_canbus()
+{
+  if (!digitalRead(CAN0_INT))
+  {
     CAN0.readMsgBuf(&rxId, &len, rxBuf);
-    if (rxId == CAN_ID_STATE_PARK_REV) {
+    if (rxId == CAN_ID_STATE_VEHICLE)
+    {
       memcpy(status.bytes, rxBuf, sizeof(status));
-      digitalWrite(park_brake_trigger_pin, status.parking_en);
-      digitalWrite(reverse_trigger_pin, ! status.reverse_en);
+
+      digitalWrite(park_brake_trigger_pin, !status.parking_en);
+      digitalWrite(out_illum_pin, !status.lights_on);
+      reverse_gear_engaged = status.reverse_en;
+
+      // Driver engaged reverse, switch to reverse even if the button is on.
+      if (reverse_gear_engaged) digitalWrite(cameras_relay, LOW);
+      // If reverse override is active then don't turn off the trigger for the reverse.
+      if (!reverse_trigger_override) digitalWrite(reverse_trigger_pin, !status.reverse_en);
+    }
+    else if (rxId == CAN_ID_STATE_SEATBELTS)
+    {
+      memcpy(seatbelts_status.bytes, rxBuf, sizeof(seatbelts_status));
+    }
+    else if (rxId == CAN_ID_STATE_ENGINE)
+    {
+      memcpy(engine_status.bytes, rxBuf, sizeof(engine_status));
+      // Serial.print("RPM: ");
+      // Serial.print(engine_status.rpm / 4);
+      // Serial.print("Speed: ");
+      // Serial.print(engine_status.speed);
+      // Serial.print(" Throttle: ");
+      // Serial.println(engine_status.throttle);
+      // speed = something
+      // odo = something
+    }
+    else if (rxId == CAN_ID_STATE_SOMETHING)
+    {
+      memcpy(something_status.bytes, rxBuf, sizeof(something_status));
     }
   }
+}
+
+void read_second_camera_button()
+{
+  // Don't do anything if reverse gear is active
+  // driver parking has priority.
+  if (reverse_gear_engaged) return;
+
+  if (camera_button_is_pressed())
+  {
+    digitalWrite(cameras_relay, HIGH);
+    digitalWrite(reverse_trigger_pin, ACTIVE);
+    reverse_trigger_override = true;
+  }
+  else
+  {
+    digitalWrite(cameras_relay, LOW);
+    reverse_trigger_override = false;
+    // Canbus will turn off the trigger.
+  }
+}
+
+void loop()
+{
+  read_second_camera_button();
+  read_canbus();
 }
